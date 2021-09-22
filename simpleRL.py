2e76168a2717da802d99496fbf7c7881395afc17
Helani Wickramaarachchi <helaniuop@gmail.com>
2021-09-22 08:02:29 +0530

    defined action

--- simpleRL.py
+++ simpleRL.py
@@ -13,6 +13,11 @@ class SampleEnvironment:
 
     def is_done(self) -> bool:
         return self.steps_left == 0
-        
-            
+
+    def action(self, action: int) -> float:
+        if self.is_done();
+            raise Exception("Game is over")
+        self.steps_left -=1
+        return random.random()
+
 

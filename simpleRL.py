e101895524b3ac5157377aa9b24b4f76ccdacbd3
Helani Wickramaarachchi <helaniuop@gmail.com>
2021-09-22 08:10:05 +0530

    completed the env class

--- simpleRL.py
+++ simpleRL.py
@@ -3,21 +3,32 @@ from typing import List
 
 class SampleEnvironment:
     def __init__(self):
+        #game have only 20 maximum steps
         self.steps_left=20
 
+    #can any no of coordinates. Info regarding the environment
     def get_observation(self) -> List(float):
         return [0.0, 0.0, 0.0]
 
+    #after performing the action agent will get 1 (+)/ 0 (-) as the reward
     def get_action(self) -> List(int):
         return [0, 1]
 
+    #whenthe no of steps have completed this will give an indication
     def is_done(self) -> bool:
         return self.steps_left == 0
 
+     #decrease the number of steps in each iteration
     def action(self, action: int) -> float:
         if self.is_done();
             raise Exception("Game is over")
         self.steps_left -=1
         return random.random()
+
+class Agent:
+    def __init__(self) -> None:
+        self.total_reward = 0.0
+
+    
 
 
